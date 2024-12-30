f21fc1b229f7966dc36c4bf05bfbc26183694b5e
Mike Odnis <airwolf635@gmail.com>
2024-12-30 16:39:26 -0500

    Quick commit

--- 2024/16/question.d.ts
+++ 2024/16/question.d.ts
@@ -1,2 +1,14 @@
-declare function DynamicParamsCurrying(...args: any): 
-<F extends any[]>(...args: F) => F[0] extends string ? never : any
+declare function DynamicParamsCurrying<F extends (...args: any[]) => boolean>(fn: F): Ret<F>;
+
+type Ret<F> =
+  F extends (...args: infer A) => infer R
+    ? A extends []
+      ? () => F
+      : <P extends unknown[]>(
+         ...args: P
+             ) => P extends A
+                 ? R
+                 : A extends [...P, ...infer Re]
+                         ? Ret<(...args: Re) => R>
+                        : never
+    : never;
